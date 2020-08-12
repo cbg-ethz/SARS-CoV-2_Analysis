ec24b89b25bae623d8d53bd1ecf4d1c3e539f627
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-08-12 13:57:28 +0200

    Update table generation

--- scripts/covid_top_positions_extended_table.R
+++ scripts/covid_top_positions_extended_table.R
@@ -21,12 +21,14 @@ n_samps <- covid_table %>% pull(SAMPLE) %>% unique() %>% length
 
 covid_table %>% filter(POS %in% c(base_df$position, 23403)) -> covid_table
 
-covid_table %>% group_by(POS) %>%
-  summarise(mut = n(), ref = unique(REF_BASE),
+covid_table %>% mutate(ENT = Entropy(A_freq) + Entropy(C_freq) +
+                     Entropy(G_freq) + Entropy(T_freq) +
+                     Entropy(DEL_freq)) %>% group_by(POS) %>%
+  summarise(mut = n(), ent = sum(ENT > 0), ref = unique(REF_BASE),
             A = mean_percent(A_freq), C = mean_percent(C_freq),
             G = mean_percent(G_freq), T = mean_percent(T_freq),
             del = mean_percent(DEL_freq)) %>%
-  mutate(mut = round(mut/n_samps*100, 2)) -> covid_table
+  mutate(mut = round(mut/n_samps*100, 2), ent = round(ent/n_samps*100, 2)) -> covid_table
 
 covid_table %>% filter(POS == 23403)
 
