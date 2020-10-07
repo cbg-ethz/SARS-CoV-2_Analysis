c63829213ac242ecbb90e16bb20494ad324450ee
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-10-07 10:08:34 +0200

    Convert tabs to spaces

--- scripts/get_base_freqs.py
+++ scripts/get_base_freqs.py
@@ -25,13 +25,13 @@ delCoverageThreshold = snakemake.config['params']['deletion_coverage_threshold']
 #f.close()
 
 #for line in lines:
-#	usedSamples.append(line.split(',')[0])
+#    usedSamples.append(line.split(',')[0])
 #print len(usedSamples)
 
 
 delFilter = ""
 if delThreshold > 0.0 or delCoverageThreshold > 0:
-	delFilter = "_delFilter_" + str(delThreshold) + "_" + str(delCoverageThreshold)
+    delFilter = "_delFilter_" + str(delThreshold) + "_" + str(delCoverageThreshold)
 
 
 outFileName = snakemake.output['fname']
@@ -44,112 +44,112 @@ fileList = glob.glob(pattern)
 
 usedFiles = 0
 for file in fileList:
-	countMuts = 0
-	sampleName = os.path.basename(file)
-	if sampleName.endswith('.vcf'):
-		sampleName  = sampleName[:-4]   # remove file extension to get sample name
-	if sampleName.startswith('snvs_'):
-		sampleName = sampleName[5:]
-	#if sampleName not in usedSamples:
-	#	print sampleName
-	#	continue
-	usedFiles += 1
+    countMuts = 0
+    sampleName = os.path.basename(file)
+    if sampleName.endswith('.vcf'):
+        sampleName  = sampleName[:-4]   # remove file extension to get sample name
+    if sampleName.startswith('snvs_'):
+        sampleName = sampleName[5:]
+    #if sampleName not in usedSamples:
+    #    print sampleName
+    #    continue
+    usedFiles += 1
     #sampleDate = file.split('/')[2]
-	posBaseCounts = {}      # pos -> base counts
-	refBaseAtPos = {}       # pos -> refBase
-	postProbs = {}
-	prevLine = ""
+    posBaseCounts = {}      # pos -> base counts
+    refBaseAtPos = {}       # pos -> refBase
+    postProbs = {}
+    prevLine = ""
 
-	with open(file) as f:
-		#print file
-		lines = f.read().splitlines()
-		for line in lines:
+    with open(file) as f:
+        #print file
+        lines = f.read().splitlines()
+        for line in lines:
 
-			if line.startswith('#'):   #skip lines not referring to a mutation
-				continue
+            if line.startswith('#'):   #skip lines not referring to a mutation
+                continue
 
              ## get the base read counts from the line
-			elems = line.split('\t')
-			pos= int(elems[1])
-			altBase = elems[4]
-			refBase = elems[3]
-			refBaseAtPos[pos] = refBase
-			info = elems[7].split(';')
-			Fvar = elems[7].split("Fvar=")[1].split(";")[0]
-			Rvar = elems[7].split("Rvar=")[1].split(";")[0]
-			Ftot = elems[7].split("Ftot=")[1].split(";")[0]
-			Rtot = elems[7].split("Rtot=")[1].split(";")[0]
-			total = int(Ftot) + int(Rtot)
-			altTotal = int(Fvar) + int(Rvar)
-			mutFreq = float(altTotal)/float(total)
+            elems = line.split('\t')
+            pos= int(elems[1])
+            altBase = elems[4]
+            refBase = elems[3]
+            refBaseAtPos[pos] = refBase
+            info = elems[7].split(';')
+            Fvar = elems[7].split("Fvar=")[1].split(";")[0]
+            Rvar = elems[7].split("Rvar=")[1].split(";")[0]
+            Ftot = elems[7].split("Ftot=")[1].split(";")[0]
+            Rtot = elems[7].split("Rtot=")[1].split(";")[0]
+            total = int(Ftot) + int(Rtot)
+            altTotal = int(Fvar) + int(Rvar)
+            mutFreq = float(altTotal)/float(total)
 
 
-			if pos not in posBaseCounts:
-				baseCounts = dict([('A', 0), ('C', 0), ('G', 0), ('T', 0), ('-', 0), ('Total', 0)])
-				posBaseCounts[pos] = baseCounts
+            if pos not in posBaseCounts:
+                baseCounts = dict([('A', 0), ('C', 0), ('G', 0), ('T', 0), ('-', 0), ('Total', 0)])
+                posBaseCounts[pos] = baseCounts
 
 
             ## some checks that read counts in multiple rows refering to same position make sense
-			if posBaseCounts[pos][altBase] != 0:
-				if posBaseCounts[pos][altBase] != altTotal:
-					print("Warning: different alt base count: " + str(posBaseCounts[pos][altBase]) + " = " + str(altTotal))
+            if posBaseCounts[pos][altBase] != 0:
+                if posBaseCounts[pos][altBase] != altTotal:
+                    print("Warning: different alt base count: " + str(posBaseCounts[pos][altBase]) + " = " + str(altTotal))
 
-			if posBaseCounts[pos]['Total'] != 0 and posBaseCounts[pos]['Total'] != total:
-				print("Warning: different total base count: " + str(posBaseCounts[pos]['Total']) + " = " + str(total))
+            if posBaseCounts[pos]['Total'] != 0 and posBaseCounts[pos]['Total'] != total:
+                print("Warning: different total base count: " + str(posBaseCounts[pos]['Total']) + " = " + str(total))
 
-			## set alt base and total read count for this position
-			posBaseCounts[pos][altBase] = altTotal
-			posBaseCounts[pos]['Total'] = total
+            ## set alt base and total read count for this position
+            posBaseCounts[pos][altBase] = altTotal
+            posBaseCounts[pos]['Total'] = total
 
-			prevLine = line
+            prevLine = line
 
 
     ## assign difference between total read count and sum of alt read counts as ref count
-	for pos in posBaseCounts:
-		refCount = posBaseCounts[pos]['Total'] - (posBaseCounts[pos]['A'] + posBaseCounts[pos]['C'] + posBaseCounts[pos]['G'] + posBaseCounts[pos]['T'] + posBaseCounts[pos]['-'])
-		posBaseCounts[pos][refBaseAtPos[int(pos)]] = refCount
+    for pos in posBaseCounts:
+        refCount = posBaseCounts[pos]['Total'] - (posBaseCounts[pos]['A'] + posBaseCounts[pos]['C'] + posBaseCounts[pos]['G'] + posBaseCounts[pos]['T'] + posBaseCounts[pos]['-'])
+        posBaseCounts[pos][refBaseAtPos[int(pos)]] = refCount
 
-		## another check that read counts make sense
-		if refCount < 0:
-			print("ERROR: NEGATIVE REF COUNT AT POSITION " + pos + " in " + file + ":")
-			print(posBaseCounts[pos])
+        ## another check that read counts make sense
+        if refCount < 0:
+            print("ERROR: NEGATIVE REF COUNT AT POSITION " + pos + " in " + file + ":")
+            print(posBaseCounts[pos])
 
 
-	## write base frequencies to output file
-	for pos, refBase in sorted(refBaseAtPos.items()):
+    ## write base frequencies to output file
+    for pos, refBase in sorted(refBaseAtPos.items()):
 
-		## check if deletion makes the threshold at this postion
-		useDel = True
-		if float(posBaseCounts[pos]['-'])/float(posBaseCounts[pos]['Total']) <= delThreshold:
-			useDel = False
-		if posBaseCounts[pos]['Total'] <= delCoverageThreshold:
-			useDel = False
+        ## check if deletion makes the threshold at this postion
+        useDel = True
+        if float(posBaseCounts[pos]['-'])/float(posBaseCounts[pos]['Total']) <= delThreshold:
+            useDel = False
+        if posBaseCounts[pos]['Total'] <= delCoverageThreshold:
+            useDel = False
 
-		## if we don't consider the deletion, update total considered read counts
-		totalConsidered = posBaseCounts[pos]['Total']
-		if not useDel:
-			totalConsidered = posBaseCounts[pos]['Total']-posBaseCounts[pos]['-']
+        ## if we don't consider the deletion, update total considered read counts
+        totalConsidered = posBaseCounts[pos]['Total']
+        if not useDel:
+            totalConsidered = posBaseCounts[pos]['Total']-posBaseCounts[pos]['-']
 
-		## check if we should skip this position
-		if totalConsidered == 0:                            # no reads left after removing deletion
-			continue
-		if posBaseCounts[pos][refBase] == totalConsidered:  # no mutation left after removing deletions
-			continue
+        ## check if we should skip this position
+        if totalConsidered == 0:                            # no reads left after removing deletion
+            continue
+        if posBaseCounts[pos][refBase] == totalConsidered:  # no mutation left after removing deletions
+            continue
 
-		# compute variant frequencies from base counts
-		baseFreqStats = str(float(posBaseCounts[pos]['A'])/float(totalConsidered)) + "\t"
-		baseFreqStats += str(float(posBaseCounts[pos]['C'])/float(totalConsidered)) + "\t"
-		baseFreqStats += str(float(posBaseCounts[pos]['G'])/float(totalConsidered)) + "\t"
-		baseFreqStats += str(float(posBaseCounts[pos]['T'])/float(totalConsidered))
-		if useDel:
-			baseFreqStats += "\t" + str(float(posBaseCounts[pos]['-'])/float(totalConsidered))
-		else:
-			baseFreqStats += "\t" + "0"
+        # compute variant frequencies from base counts
+        baseFreqStats = str(float(posBaseCounts[pos]['A'])/float(totalConsidered)) + "\t"
+        baseFreqStats += str(float(posBaseCounts[pos]['C'])/float(totalConsidered)) + "\t"
+        baseFreqStats += str(float(posBaseCounts[pos]['G'])/float(totalConsidered)) + "\t"
+        baseFreqStats += str(float(posBaseCounts[pos]['T'])/float(totalConsidered))
+        if useDel:
+            baseFreqStats += "\t" + str(float(posBaseCounts[pos]['-'])/float(totalConsidered))
+        else:
+            baseFreqStats += "\t" + "0"
 
-		posInfo = sampleName + "\t" + str(pos) + "\t" + refBaseAtPos[pos]
-		outFile.write( posInfo + "\t" + str(totalConsidered) + "\t" + baseFreqStats + "\n")
-		countMuts += 1
-	#print str(countMuts) + "\t" + sampleName
+        posInfo = sampleName + "\t" + str(pos) + "\t" + refBaseAtPos[pos]
+        outFile.write( posInfo + "\t" + str(totalConsidered) + "\t" + baseFreqStats + "\n")
+        countMuts += 1
+    #print str(countMuts) + "\t" + sampleName
 print(usedFiles)
 
 outFile.close()
