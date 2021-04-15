bf55036a81ee9eb00b56e2e210bdee7508972e32
kpj <kim.philipp.jablonski@gmail.com>
2021-04-15 17:08:26 +0200

    Format code

--- workflow/scripts/get_base_freqs.py
+++ workflow/scripts/get_base_freqs.py
@@ -19,19 +19,36 @@ from tqdm import tqdm
 INDEL_COLS = 10
 
 
-def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
-            inThreshold=0, indelCoverageThreshold=0):
+def get_base_freqs(
+    fname_list,
+    fname_samples,
+    out_file,
+    delThreshold=0,
+    inThreshold=0,
+    indelCoverageThreshold=0,
+):
 
     df_samples = pd.read_csv(fname_samples)
     usedSamples = df_samples["accession"].tolist()
     print(f"Parsing {len(usedSamples)}/{len(fname_list)} samples")
 
-    cols = ["SAMPLE", "POS", "REF_BASE", "ALT_BASE", "(ADJUSTED_)READ_COUNT",
-        "A_freq", "C_freq", "G_freq", "T_freq", "DEL_freq", "IN_freq"]
+    cols = [
+        "SAMPLE",
+        "POS",
+        "REF_BASE",
+        "ALT_BASE",
+        "(ADJUSTED_)READ_COUNT",
+        "A_freq",
+        "C_freq",
+        "G_freq",
+        "T_freq",
+        "DEL_freq",
+        "IN_freq",
+    ]
     cols.extend([f"IN{i}_freq" for i in range(1, INDEL_COLS + 1, 1)])
     print(out_file)
     out_stream = open(out_file, "w")
-    out_stream.write('\t'.join(cols) + '\n')
+    out_stream.write("\t".join(cols) + "\n")
 
     # all files matching this pattern are processed
     # pattern = "samples/*/*/variants/SNVs/snvs.vcf"
@@ -61,19 +78,23 @@ def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
                 # Get the base read counts from the line
                 elems = line.split("\t")
 
-                assert int(elems[1]) >= pos, 'VCF file needs to be sorted'
+                assert int(elems[1]) >= pos, "VCF file needs to be sorted"
 
                 pos = int(elems[1])
                 refBase = elems[3]
                 altBase = elems[4]
-                
+
                 # LoFreq input
                 if "DP4" in elems[7]:
                     info = elems[7].split(";")
                     # Counts for ref-forward bases, ref-reverse, alt-forward,
                     #   and alt-reverse bases
-                    counts = [int(j) for i in info for j in i[4:].split(",")
-                        if i.startswith("DP4=")]
+                    counts = [
+                        int(j)
+                        for i in info
+                        for j in i[4:].split(",")
+                        if i.startswith("DP4=")
+                    ]
                     Fvar = counts[2]
                     Rvar = counts[3]
                     Ftot = Fvar + counts[0]
@@ -92,23 +113,39 @@ def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
                 if len(refBase) > len(altBase):
                     for i_pos, i_base in enumerate(refBase[1:], 1):
                         try:
-                            baseCounts[pos + i_pos]['-'] += altTotal
-                            baseCounts[pos + i_pos]['total'] += total
+                            baseCounts[pos + i_pos]["-"] += altTotal
+                            baseCounts[pos + i_pos]["total"] += total
                         except KeyError:
-                            baseCounts[pos + i_pos] = {'A': 0, 'C': 0, 'G': 0,
-                                'T': 0, '-': altTotal, '+': 0, 'total': total,
-                                'ref': i_base, 'alt': ['-']}
+                            baseCounts[pos + i_pos] = {
+                                "A": 0,
+                                "C": 0,
+                                "G": 0,
+                                "T": 0,
+                                "-": altTotal,
+                                "+": 0,
+                                "total": total,
+                                "ref": i_base,
+                                "alt": ["-"],
+                            }
                     continue
                 # Mutation, Insertion, or Deletion in SHoRaH
                 else:
                     try:
-                        baseCounts[pos]['ref'] = refBase
-                        baseCounts[pos]['alt'].append(altBase)
+                        baseCounts[pos]["ref"] = refBase
+                        baseCounts[pos]["alt"].append(altBase)
                     # New position
                     except KeyError:
-                        baseCounts[pos] = {'A': 0, 'C': 0, 'G': 0, 'T': 0,
-                            '-': 0, '+': 0, 'total': total, 'ref': refBase,
-                            'alt': [altBase]}
+                        baseCounts[pos] = {
+                            "A": 0,
+                            "C": 0,
+                            "G": 0,
+                            "T": 0,
+                            "-": 0,
+                            "+": 0,
+                            "total": total,
+                            "ref": refBase,
+                            "alt": [altBase],
+                        }
                         # Insertion (LoFreq and ShoRaH)
                         if len(altBase) > len(refBase):
                             altBase = "+"
@@ -117,27 +154,30 @@ def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
                     else:
                         # Insertion (LoFreq and ShoRaH)
                         if len(altBase) > len(refBase):
-                            in_no = sum([len(i) > 1 \
-                                for i in baseCounts[pos]['alt'][:-1]])
+                            in_no = sum(
+                                [len(i) > 1 for i in baseCounts[pos]["alt"][:-1]]
+                            )
                             # If there is already an insetion, add another col
                             if in_no > 0:
                                 altBase = f"+{in_no}"
                                 baseCounts[pos][altBase] = 0
                             else:
                                 altBase = "+"
-                            
-                        baseCounts[pos][altBase] += altTotal               
+
+                        baseCounts[pos][altBase] += altTotal
                         baseCounts[pos]["total"] += total
 
                 # Checks that read counts in refering to same position make sense
                 if baseCounts[pos][altBase] != 0:
                     # Only deletion events: can overlap
-                    if all(['-' == i for i in baseCounts[pos]['alt']]):
+                    if all(["-" == i for i in baseCounts[pos]["alt"]]):
                         pass
                     # Different events: shouldnt overlap
                     elif baseCounts[pos][altBase] != altTotal:
-                        print(f"Warning: pos {pos}: different alt   base count: " \
-                            f"{baseCounts[pos][altBase]} = {altTotal}")
+                        print(
+                            f"Warning: pos {pos}: different alt   base count: "
+                            f"{baseCounts[pos][altBase]} = {altTotal}"
+                        )
 
                 # DEPRECATED for LoFreq input
                 # if total != baseCounts[pos]['total'] != 0:
@@ -147,15 +187,21 @@ def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
         # Assign difference between total read count and sum of alt read counts as ref count
         for pos, pos_data in sorted(baseCounts.items()):
             # Check if more than 1 deletion was called
-            in_no = sum([len(i) > 1 for i in pos_data['alt']])
-            altCounts = pos_data["A"] + pos_data["C"] + pos_data["G"] \
-                + pos_data["T"] + pos_data["-"] + pos_data["+"]
-            for in_i in range(1, in_no, 1): 
+            in_no = sum([len(i) > 1 for i in pos_data["alt"]])
+            altCounts = (
+                pos_data["A"]
+                + pos_data["C"]
+                + pos_data["G"]
+                + pos_data["T"]
+                + pos_data["-"]
+                + pos_data["+"]
+            )
+            for in_i in range(1, in_no, 1):
                 altCounts += pos_data[f"+{in_i}"]
-            pos_data[pos_data['ref']] = pos_data["total"] - altCounts
+            pos_data[pos_data["ref"]] = pos_data["total"] - altCounts
 
             # Check that read counts make sense
-            if pos_data[pos_data['ref']] < 0:
+            if pos_data[pos_data["ref"]] < 0:
                 print(f"ERROR: NEGATIVE REF COUNT AT POSITION {pos} in {file}:")
                 print(baseCounts[pos])
 
@@ -175,77 +221,91 @@ def get_base_freqs(fname_list, fname_samples, out_file, delThreshold=0,
             if not useDel:
                 pos_data["total"] -= pos_data["-"]
                 pos_data["-"] = 0
-                pos_data['alt'] = [i for i in pos_data['alt'] if i != '-']
+                pos_data["alt"] = [i for i in pos_data["alt"] if i != "-"]
             # If insertion is not considered, update total considered read counts
             if not useIn:
                 pos_data["total"] -= pos_data["+"]
                 pos_data["+"] = 0
-                pos_data['alt'] = [i for i in pos_data['alt'] if i != '+']
+                pos_data["alt"] = [i for i in pos_data["alt"] if i != "+"]
 
             # Skip position if no reads are left
-            if pos_data["total"] == 0:  
+            if pos_data["total"] == 0:
                 continue
             # Skip position if no SNP reads are left
-            if pos_data[pos_data['ref']] == pos_data["total"]:
+            if pos_data[pos_data["ref"]] == pos_data["total"]:
                 continue
 
             # compute variant frequencies from base counts
-            new_line = [sampleName, pos, pos_data["ref"],
-                ','.join(pos_data["alt"]), pos_data["total"],
+            new_line = [
+                sampleName,
+                pos,
+                pos_data["ref"],
+                ",".join(pos_data["alt"]),
+                pos_data["total"],
                 pos_data["A"] / pos_data["total"],
                 pos_data["C"] / pos_data["total"],
                 pos_data["G"] / pos_data["total"],
                 pos_data["T"] / pos_data["total"],
                 pos_data["-"] / pos_data["total"],
-                pos_data["+"] / pos_data["total"]]
+                pos_data["+"] / pos_data["total"],
+            ]
             new_line.extend(INDEL_COLS * [0])
             # Check if new insertion line was added
             if 1 < in_no <= INDEL_COLS:
                 # Iterate over second, third/ fourth... insertion
                 for in_i in range(1, in_no, 1):
-                    new_line[10 + in_i] = \
-                        pos_data[f"+{in_i}"] / pos_data["total"]
+                    new_line[10 + in_i] = pos_data[f"+{in_i}"] / pos_data["total"]
             elif in_no > INDEL_COLS:
                 print(f"ERROR: >{INDEL_COLS} INSERTIONS AT POS {pos} IN {file}:")
                 print(baseCounts[pos])
 
-            out_stream.write('\t'.join([str(i) for i in new_line]) + '\n')
+            out_stream.write("\t".join([str(i) for i in new_line]) + "\n")
 
     out_stream.close()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('fname_list', type=str, nargs='+')
-    parser.add_argument('-s', '--fname_samples', type=str, default='')
-    parser.add_argument('-o', '--out_file', type=str, default='test_out.csv')
-    parser.add_argument('-d', '--delThreshold', type=float, default=0)
-    parser.add_argument('-i', '--inThreshold', type=float, default=0)
-    parser.add_argument('-c', '--indelCoverageThreshold', type=int, default=0)
+    parser.add_argument("fname_list", type=str, nargs="+")
+    parser.add_argument("-s", "--fname_samples", type=str, default="")
+    parser.add_argument("-o", "--out_file", type=str, default="test_out.csv")
+    parser.add_argument("-d", "--delThreshold", type=float, default=0)
+    parser.add_argument("-i", "--inThreshold", type=float, default=0)
+    parser.add_argument("-c", "--indelCoverageThreshold", type=int, default=0)
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
-    if 'snakemake' in globals():
-        get_base_freqs(snakemake.input["fname_list"],
-            snakemake.input["fname_samples"], snakemake.output["fname"],
+if __name__ == "__main__":
+    if "snakemake" in globals():
+        get_base_freqs(
+            snakemake.input["fname_list"],
+            snakemake.input["fname_samples"],
+            snakemake.output["fname"],
             delThreshold=snakemake.config["params"]["deletion_threshold"],
             inThreshold=snakemake.config["params"]["deletion_threshold"],
-            indelCoverageThreshold=snakemake \
-                .config["params"]["deletion_coverage_threshold"])
+            indelCoverageThreshold=snakemake.config["params"][
+                "deletion_coverage_threshold"
+            ],
+        )
     else:
         import argparse
+
         args = parse_args()
         if not args.fname_samples:
             import tempfile
+
             samples = tempfile.NamedTemporaryFile(delete=False)
-            sample_list = [os.path.basename(i).replace('.vcf', '') \
-                for i in args.fname_list]
-            samples.write(str.encode('accession\n' + '\n'.join(sample_list)))
+            sample_list = [
+                os.path.basename(i).replace(".vcf", "") for i in args.fname_list
+            ]
+            samples.write(str.encode("accession\n" + "\n".join(sample_list)))
             samples.close()
             args.fname_samples = samples.name
-        get_base_freqs(args.fname_list, args.fname_samples, args.out_file,
+        get_base_freqs(
+            args.fname_list,
+            args.fname_samples,
+            args.out_file,
             delThreshold=args.delThreshold,
-            indelCoverageThreshold=args.indelCoverageThreshold)
-        
+            indelCoverageThreshold=args.indelCoverageThreshold,
+        )
